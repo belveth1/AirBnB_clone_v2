dce297645677a64867d508d451ddfea521b9ffac
Ueudje <classlol72@gmail.com>
2024-01-17 13:24:00 -0800

    done

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """ document documt """
 from tests.test_models.test_base_model import test_basemodel
-from models.city import City
+from models.state import State
 
 
-class test_City(test_basemodel):
+class test_state(test_basemodel):
     """ document documt """
 
     def __init__(self, *args, **kwargs):
         """ document documt """
         super().__init__(*args, **kwargs)
-        self.name = "City"
-        self.value = City
+        self.name = "State"
+        self.value = State
 
-    def test_state_id(self):
-        """ document documt """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
-
-    def test_name(self):
+    def test_name3(self):
         """ document documt """
         new = self.value()
         self.assertEqual(type(new.name), str)
