5000b67c4f4e9b9f4ce681edef55c26069b58a35
Ueudje <classlol72@gmail.com>
2024-01-20 17:13:49 -0800

    done

--- models/city.py
+++ models/city.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models import ValueEnv
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    if ValueEnv == "db":
-        __tablename__ = 'cities'
+    """ The city class, contains state ID & name """
+    __tablename__ = "cities"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all,delete")
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete, delete-orphan")
     else:
-        name = ""
         state_id = ""
+        name = ""
--- models/state.py
+++ models/state.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models import ValueEnv
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    if ValueEnv == "db":
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade='all, delete')
-    else:
-        name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",
+                          backref="state", cascade="all, delete,delete-orphan")
 
+    if getenv('HBNB_TYPE_STORAGE') != "db":
         @property
         def cities(self):
-            """returns the list of Cities"""
-            from models.city import City
-            from models.__init__ import storage
-            List_cities = []
-            ALLcities = storage.all(City)
-            for city in ALLcities.values():
+            """ returns list of City instances related to state """
+            from models import storage
+            list_cities = []
+            for city in storage.all(City).values():
                 if city.state_id == self.id:
-                    List_cities.append(city)
-            return List_cities
+                    list_cities.append(city)
+            return list_cities
