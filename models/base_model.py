002378cc274122dc012652b49531403b3a0df104
sherif mostafa <sherifmostafa765@gmail.com>
2024-06-29 20:43:39 +0000

    s

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import os
@@ -29,14 +30,13 @@ class BaseModel:
                         setattr(self, key, datetime.fromisoformat(value))
                     else:
                         setattr(self, key, value)
-            
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
-
+            # if os.getenv('HBNB_TYPE_STORAGE') in ('db'):
+            if not hasattr(kwargs, 'id'):
+                setattr(self, 'id', str(uuid.uuid4()))
+            if not hasattr(kwargs, 'created_at'):
+                setattr(self, 'created_at', datetime.now())
+            if not hasattr(kwargs, 'updated_at'):
+                setattr(self, 'updated_at', datetime.now())
 
     def __str__(self):
         """Returns a string representation of the instance"""
