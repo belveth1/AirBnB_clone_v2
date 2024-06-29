bb9c412b60eb994e03de07c355323fa22b3ead28
sherif mostafa <sherifmostafa765@gmail.com>
2024-06-29 20:50:14 +0000

    s

--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This module instantiates an object of class FileStorage"""
+import os
 
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+storage = DBStorage() if os.getenv(
+    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
+"""A unique FileStorage/DBStorage instance for all models.
+"""
+storage.reload()
